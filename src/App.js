b984707498f6254eae5bb71e3c62729e1137af7d
noahbei <nbeidelman9@gmail.com>
2024-02-14 13:10:15 -0800

    change name to dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Homepage from './components/Homepage'
+import Dashboard from './components/Dashboard'
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Dashboard />
     </div>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Homepage = () => {
+const Dashboard = () => {
   return (
     <div>
       <header>
-        <h1>My Simple React Home Page</h1>
+        <h1>Dashboard</h1>
       </header>
       <main>
-        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+        <p>different blobs here</p>
         <img src="https://via.placeholder.com/300" alt="Placeholder" />
       </main>
     </div>
   );
 }
 
-export default Homepage;
+export default Dashboard;
