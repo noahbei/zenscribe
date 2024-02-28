592a30dc10b7e72d72e16fc8c9196ea042127fca
Noah Beidelman <nbeidelman9@gmail.com>
2024-02-27 23:49:17 -0800

    rename pages, add more pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contact = () => {
+  const contactStyle = {
+    marginLeft: "200px",
+    padding: "0 20px"
+  }
+
+  return (
+    <div style={contactStyle}>
+      <header>
+        <h1>Contact</h1>
+      </header>
+      <main>
+        <p>contact page here</p>
+      </main>
+    </div>
+  );
+}
+
+export default Contact;
