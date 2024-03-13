c9142412a100fe18939c29115d8fd9d90d33cb94
noahbei <nbeidelman9@gmail.com>
2024-03-13 12:33:48 -0700

    initialize firestore

---
+++ src/database/firestore.js
@@ -0,0 +1,6 @@
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore';
+
+initializeApp();
+
+const db = getFirestore();
