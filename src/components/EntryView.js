6e66c9c230162419211243555be87534f7214be5
noahbei <nbeidelman9@gmail.com>
2024-04-10 13:03:52 -0700

    add truncated entry

--- src/components/EntryView.js
+++ src/components/EntryView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/EntryView.css';
 
 const EntryView = ({ children, date, onClick }) => {
-  const truncatedChildren = children.slice(0, 100);
+  const truncatedChildren = children.length > 350 ? children.slice(0, 350) + ". . ." : children;
   return (
     <div className="entry-view" onClick={onClick}>
       <div>{date}</div>
