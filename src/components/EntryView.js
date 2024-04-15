77404cb1d675459abfa004c0074bc0fa2ad9335e
noahbei <nbeidelman9@gmail.com>
2024-04-15 12:12:44 -0700

    add expanding functionality to entry page

--- src/components/EntryView.js
+++ src/components/EntryView.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/EntryView.css';
+import { child } from 'firebase/database';
 
 const EntryView = ({ children, date, onClick }) => {
-  const truncatedChildren = children.length > 350 ? children.slice(0, 350) + ". . ." : children;
+  const [isExpanded, setIsExpanded] = useState(false)
+  let truncatedChildren = children.length > 350 ? children.slice(0, 350) + ". . ." : children;
+  const handleClick = () => {
+    setIsExpanded(!isExpanded)
+  }
+
   return (
-    <div className="entry-view" onClick={onClick}>
+    <div className="entry-view" onClick={handleClick}>
       <div>{date}</div>
-      <p>{truncatedChildren}</p>
+      <p>{isExpanded ? children : truncatedChildren}</p>
     </div>
   );
 }
