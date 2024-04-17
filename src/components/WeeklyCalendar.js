651c1b39bbbe62994e30cfef1646f44e52669b01
Noah Beidelman <nbeidelman9@gmail.com>
2024-04-17 01:05:09 -0700

    add weekly calendar working

--- src/components/WeeklyCalendar.js
+++ src/components/WeeklyCalendar.js
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/WeeklyCalendar.css';
 
-const WeeklyCalendar = () => {
+const WeeklyCalendar = ({ entries }) => {
   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  // Ratings need to be updated later on per user account
-  const [ratings, setRatings] = useState([5, 6, 10, 8, 9, 2, 6]);
+  const [ratings, setRatings] = useState(Array(7).fill(0));
 
-  const handleRatingChange = (index, rating) => {
-    const newRatings = [...ratings];
-    newRatings[index] = rating;
-    setRatings(newRatings);
-  };
+  useEffect(() => {
+    if (entries.length > 0) {
+      const today = new Date();
+      const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay()));
+      const endOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 6));
+
+      const dailyRatings = Array(7).fill(0);
+      const countPerDay = Array(7).fill(0);
+
+      entries.forEach(entry => {
+        const entryDate = new Date(entry.createdAt.replace(" at ", " "));
+        if (entryDate >= startOfWeek && entryDate <= endOfWeek) {
+          const dayIndex = entryDate.getDay();
+          dailyRatings[dayIndex] += entry.rating;
+          countPerDay[dayIndex]++;
+        }
+      });
+
+      setRatings(dailyRatings.map((totalRating, index) => {
+        const count = countPerDay[index];
+        return count === 0 ? 0 : totalRating / count;
+      }));
+    }
+  }, [entries]);
 
   return (
     <div className="weekly-calendar">
       <div className="chart">
         {ratings.map((rating, index) => (
-          <div key={index} className="chart-bar" style={{ height: `${rating * 25}px` }} onClick={() => handleRatingChange(index, rating)}>
-            {rating}
+          <div key={index} className="chart-bar" style={{ height: `${rating * 25}px` }}>
+            {rating.toFixed(2)}
           </div>
         ))}
       </div>
