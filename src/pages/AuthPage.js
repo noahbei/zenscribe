b8fe31eee163b77f23899b5fd7e27d8b94c57b7a
noahbei <nbeidelman9@gmail.com>
2024-05-01 12:46:59 -0700

    remove user auth test

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -89,8 +89,6 @@ const AuthPage = () => {
       </form>
       <button onClick={signInWithGoogle}>Sign in with google</button>
       <button onClick={logout}>logout</button>
-      <p>Current Account: <span>{auth?.currentUser?.email}</span></p>
-
       <p>
         {isLogin ? 'Don\'t have an account?' : 'Already have an account?'}
         <Link to="#" onClick={toggleAuthMode}>
