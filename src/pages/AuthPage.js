af6107512a3087055b18830d43a7853b8524af45
Noah Beidelman <nbeidelman9@gmail.com>
2024-05-04 21:23:32 -0700

    initial auth page styling

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../logo.svg';
+import { ReactComponent as Title } from '../zenscribe-title.svg'
 import { auth, googleProvider } from '../config/firebase';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
 import { Navigate } from 'react-router-dom';
+const Separator = () => (
+  <div style={{ display: 'flex', alignItems: 'center', width: '100%', margin: '10px 0' }}>
+    <div style={{ flex: 1, height: '1px', backgroundColor: 'black' }}></div>
+    <div style={{ margin: '0 10px', color: 'black' }}>or</div>
+    <div style={{ flex: 1, height: '1px', backgroundColor: 'black' }}></div>
+  </div>
+);
+
 
 const AuthPage = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -54,11 +63,16 @@ const AuthPage = () => {
     margin: 'auto',
   };
 
-  const Logostyle = {
-    width: '250px', 
-    height: '250px',
-    marginBottom: '1px',
-    
+  const LogoStyle = {
+    position: "fixed",
+    left: 0,
+    top: 0,
+    height: "200px",
+    width: "200px"
+  }
+
+  const TitleStyle = {
+    maxWidth: '800px'
   }
 
   if (redirectToHome) {
@@ -67,7 +81,9 @@ const AuthPage = () => {
 
   return (
     <div style={authPageStyle}>
-      <Logo style ={Logostyle} />
+      <Title style={TitleStyle}/>
+      <Logo style={LogoStyle}/>
+      <div>
       <h2>{isLogin ? 'Login' : 'Signup'}</h2>
       <form onSubmit={handleSubmit}>
         <div>
@@ -94,15 +110,18 @@ const AuthPage = () => {
         </div>
 
         <button type="submit">{isLogin ? 'Login' : 'Signup'}</button>
+        <button onClick={logout}>logout</button>
       </form>
+      <Separator/>
       <button onClick={signInWithGoogle}>Sign in with google</button>
-      <button onClick={logout}>logout</button>
+      
       <p>
         {isLogin ? 'Don\'t have an account?' : 'Already have an account?'}
         <Link to="#" onClick={toggleAuthMode}>
           {isLogin ? 'Signup' : 'Login'}
         </Link>
       </p>
+      </div>
     </div>
   );
 };
