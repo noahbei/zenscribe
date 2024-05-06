29b7edd80e7461b5ad069df2b2983086bbeb92ab
noahbei <nbeidelman9@gmail.com>
2024-05-06 10:56:58 -0700

    fix logout routing

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/Button.css';
 
-const Button = ({ children, onClick, style }) => {
+const Button = ({ children, onClick, style, type }) => {
   return (
-    <button className="button" onClick={onClick} style={style}>
+    <button type={type} className="button" onClick={onClick} style={style}>
       {children}
     </button>
   );
