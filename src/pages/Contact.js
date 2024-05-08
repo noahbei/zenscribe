f52b66016541906e09a47ad7e253bc242ccd06f7
Noah Beidelman <nbeidelman9@gmail.com>
2024-05-07 22:54:13 -0700

    fix opacity issue, adjust contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,16 +7,27 @@ const Contact = () => {
     padding: "0 20px"
   }
 
+  const innerStyle = {
+    backgroundColor: "white",
+    padding: "20px",
+    paddingTop: "0px",
+    margin: "20px",
+    borderRadius: "15px",
+    display: "inline-block"
+  }
+
   return (
     <div className={'container-style'} style={contactStyle}>
-      <PageTitle>Contact Us</PageTitle>
-      <p>Feel free to reach out to us using the information below:</p>
+      <div style={innerStyle}>
+        <PageTitle>Contact Us</PageTitle>
+        <p>Feel free to reach out to us using the information below:</p>
 
-      <div>
-        <h2>Contact Information</h2>
-        <p>Email: zenscribe@gmail.com</p>
-        <p>Phone: 123-456-7890</p>
-        <p>Address: 800 N. State College Blvd</p>
+        <div>
+          <h2>Contact Information</h2>
+          <p>Email: zenscribe@gmail.com</p>
+          <p>Phone: 123-456-7890</p>
+          <p>Address: 800 N. State College Blvd</p>
+        </div>
       </div>
     </div>
   );
